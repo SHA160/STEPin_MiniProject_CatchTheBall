083ddb75cc50470c21886c29fc46c9c0311cb278
SHA160 <49854369+SHA160@users.noreply.github.com>
2021-09-07 23:15:20 +0530

    Add files via upload

---
+++ 3_Implementation/DisplayBorder.h
@@ -0,0 +1,26 @@
+/**
+ * @file DisplayBorder.h
+ * @author SHA160
+ * @brief DisplayBorder structure definition
+ * @date 2021-08-30
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ * @note Do not upload this file to submitty as submitty already has this file
+ *          and will create error if uploaded
+ * 
+ */
+
+
+#ifndef _DisplayBorder_H
+#define _DisplayBorder_H
+
+#include <stdio.h>          //defines three variable types, several macros, and various functions for performing input and output.
+#include <Windows.h>        //used to access the Win32 API functions and it makes it easier for the user to use the in-built functionality.
+#include <conio.h>          //includes inbuilt functions like getch() and clrscr()
+#include <stdlib.h>         //defines four variable types, several macros, and various functions for performing general functions.
+
+void DisplayBorder();
+void setxy (int x, int y);
+
+#endif
